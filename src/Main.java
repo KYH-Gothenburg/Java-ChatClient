ca429a550b17d850794ad8b78771a7cd7997e2d4
Joakim Wassberg <joakim@arthead.se>
2021-12-07 10:58:26 +0100

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            // Create a socket and connect to the server
+            Socket clientSocket = new Socket("localhost", 9876);
+
+            // Create input and output streams for communication with the server
+            BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            PrintWriter output = new PrintWriter(clientSocket.getOutputStream(), true);
+            // We need a scanner so we can get some user input
+            Scanner in = new Scanner(System.in);
+            // We store the user input in inData
+            String inData;
+            do {
+                // Get user input
+                inData = in.next();
+                // Send the suer input to the server
+                output.println(inData);
+
+                // We will only receive data from the server if we have sent something other than exit
+                if(!inData.equals("exit")) {
+                    // Get data from server
+                    String message = input.readLine();
+                    // and print it
+                    System.out.println(message);
+                }
+
+                // If we wrote exit, we will now exit the loop and exit the application
+            } while(!inData.equals("exit"));
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
